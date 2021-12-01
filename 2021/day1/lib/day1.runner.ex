90c8c47ef9f6a6ae8c3f431473ef751d9509e980
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-01 14:51:39 +0200

    reformat code with mix format

--- 2021/day1/lib/day1.runner.ex
+++ 2021/day1/lib/day1.runner.ex
@@ -3,7 +3,8 @@ defmodule Day1Runner do
     case File.read("data.txt") do
       {:ok, contents} -> process(contents)
       {:error, reason} -> "failed to read file 'data.txt': #{reason}"
-    end |> IO.puts
+    end
+    |> IO.puts()
   end
 
   def process(contents) do
