9644994f73004928b054fd5939e7daa14b5588f1
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-07 19:04:29 +0200

    day7: task2

--- 2021/day7/test/day7_test.exs
+++ 2021/day7/test/day7_test.exs
@@ -1,8 +1,11 @@
 defmodule Day7Test do
   use ExUnit.Case
-  doctest Task1
 
   test "Task 1 solution test" do
-    assert Task1.solution([16,1,2,0,4,2,7,1,2,14]) == 37
+    assert Task1.solution([16, 1, 2, 0, 4, 2, 7, 1, 2, 14]) == 37
+  end
+
+  test "Task 2 solution test" do
+    assert Task2.solution([16, 1, 2, 0, 4, 2, 7, 1, 2, 14]) == 168
   end
 end
