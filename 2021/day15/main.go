87780ac517a0aa15faa02a1b411004f4b71d6df6
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-15 15:12:52 +0200

    day15: run both tasks in main

--- 2021/day15/main.go
+++ 2021/day15/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"aoc-2021-day15/graph"
+	"aoc-2021-day15/task1"
 	"aoc-2021-day15/task2"
 	"fmt"
 )
 
 func main() {
-	//dataT1, err := graph.ParseInputForTask1("data.txt")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//t1, err := task1.Solve(dataT1)
-	//fmt.Printf("task 1 answer: %v, err: %v\n", t1, err)
+	dataT1, err := graph.ParseInputForTask1("data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	t1, err := task1.Solve(dataT1)
+	fmt.Printf("task 1 answer: %v, err: %v\n", t1, err)
 
 	dataT2, err := graph.ParseInputForTask2("data.txt")
 	if err != nil {
