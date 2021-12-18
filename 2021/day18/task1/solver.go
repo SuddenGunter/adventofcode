d23303602f0993b4528c5840994eccf9712e37df
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-18 17:35:35 +0200

    day18: task1 sum

--- 2021/day18/task1/solver.go
+++ 2021/day18/task1/solver.go
@@ -1,7 +1,25 @@
 package task1
 
-import "aoc-2021-day18/input"
+import (
+	"aoc-2021-day18/input"
+	"aoc-2021-day18/tree"
+	"fmt"
+)
 
 func Solve(data input.Data) (int, error) {
+	root := data.Numbers[0]
+	for _, n := range data.Numbers[1:] {
+		fmt.Printf("%v + %v = ", root, n)
+		root = sum(root, n)
+		fmt.Printf("%v\n", root)
+	}
 	return 0, nil
+}
+
+func sum(root tree.Node, n tree.Node) tree.Node {
+	return &tree.PairNode{
+		Parent:     nil,
+		LeftChild:  root,
+		RightChild: n,
+	}
 }
