e49fe53b5804f8a072ad9ec2a65f7b83badc71a8
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-20 13:18:10 +0200

    day20: task1

--- 2021/day20/task1/solver.go
+++ 2021/day20/task1/solver.go
@@ -4,6 +4,7 @@ import (
 	"aoc-2021-day20/image"
 	"aoc-2021-day20/image/algorithm"
 	"aoc-2021-day20/input"
+	"fmt"
 )
 
 func Solve(data input.Data) (int, error) {
@@ -12,16 +13,42 @@ func Solve(data input.Data) (int, error) {
 		return 0, err
 	}
 
-	step1 := enhance(resized, data.Algorithm)
+	fmt.Println("original image")
+	prettyPrint(resized)
+	fmt.Println()
 
-	resizedS1, err := image.FromExisting(step1, len(step1.Pixels)+4, len(step1.Pixels[0])+4, 2, 2)
+	step0 := enhance(resized, data.Algorithm, 0)
+
+	fmt.Println("step0")
+	prettyPrint(step0)
+	fmt.Println()
+
+	resizedS1, err := image.FromExisting(step0, len(step0.Pixels)+4, len(step0.Pixels[0])+4, 2, 2)
 	if err != nil {
 		return 0, err
 	}
 
-	step2 := enhance(resizedS1, data.Algorithm)
+	step1 := enhance(resizedS1, data.Algorithm, 1)
 
-	return calculateLit(step2), nil
+	fmt.Println("step1")
+	prettyPrint(step1)
+	fmt.Println()
+
+	return calculateLit(step1), nil
+}
+
+func prettyPrint(img image.Image) {
+	for i := range img.Pixels {
+		for _, v := range img.Pixels[i] {
+			if v == 1 {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf(".")
+			}
+		}
+
+		fmt.Println()
+	}
 }
 
 func calculateLit(img image.Image) int {
@@ -35,17 +62,17 @@ func calculateLit(img image.Image) int {
 	return sum
 }
 
-func enhance(resized image.Image, algorithm algorithm.Algorithm) image.Image {
+func enhance(resized image.Image, algorithm algorithm.Algorithm, step int) image.Image {
 	center := position{
-		x: 1,
-		y: 1,
+		x: 0,
+		y: 0,
 	}
 
 	newImage := resized.Clone()
 
-	for ; center.x < len(resized.Pixels)-1; center.x++ {
-		for ; center.y < len(resized.Pixels[0])-1; center.y++ {
-			number := getNumericVal(resized, center)
+	for ; center.x < len(resized.Pixels); center.x++ {
+		for ; center.y < len(resized.Pixels[0]); center.y++ {
+			number := getNumericVal(resized, algorithm, center, step)
 			algorithmVal := algorithm[number]
 			newImage.Pixels[center.x][center.y] = algorithmVal
 		}
@@ -56,17 +83,36 @@ func enhance(resized image.Image, algorithm algorithm.Algorithm) image.Image {
 	return newImage
 }
 
-func getNumericVal(resized image.Image, center position) int {
+func getNumericVal(resized image.Image, alg algorithm.Algorithm, center position, step int) int {
 	num := 0
 
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
 			num = num << 1
-			num += int(resized.Pixels[center.x+i][center.y+j])
+			num += getVal(resized, alg, center.x+i, center.y+j, step)
 		}
 	}
 
 	return num
+}
+
+func getVal(resized image.Image, alg algorithm.Algorithm, i int, j int, step int) int {
+	if i < 0 || i >= len(resized.Pixels) || j < 0 || j >= len(resized.Pixels[0]) {
+		switch {
+		case step == 0:
+			return 0
+		case step%2 == 0:
+			return int(alg[0])
+		default:
+			if alg[0] == 0 {
+				return 0
+			} else {
+				return int(alg[511])
+			}
+		}
+	} else {
+		return int(resized.Pixels[i][j])
+	}
 }
 
 type position struct {
