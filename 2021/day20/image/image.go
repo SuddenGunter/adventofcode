ce7795efc04b879b9a5bf45c971fa86dfd1eff7e
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-20 11:57:56 +0200

    day20: implement image copy

--- 2021/day20/image/image.go
+++ 2021/day20/image/image.go
@@ -6,6 +6,33 @@ type Image struct {
 	Pixels [][]byte
 }
 
+// FromExisting creates new image with sizeX x sizeY and places top left corner of the old
+// image to the [posX;posY].
+func FromExisting(img Image, sizeX, sizeY, posX, posY int) (Image, error) {
+	if sizeX < len(img.Pixels)+posX {
+		return Image{}, errors.New("can't fit big image to small canvas")
+	}
+
+	pixels := make([][]byte, 0, sizeX)
+	for i := 0; i < sizeY; i++ {
+		line := make([]byte, sizeY)
+		pixels = append(pixels, line)
+	}
+
+	for i := range img.Pixels {
+
+		if sizeY < len(img.Pixels[i])+posY {
+			return Image{}, errors.New("can't fit big image to small canvas")
+		}
+
+		for j := range img.Pixels[i] {
+			pixels[i+posX][j+posY] = img.Pixels[i][j]
+		}
+	}
+
+	return Image{Pixels: pixels}, nil
+}
+
 func FromLines(lines []string) (Image, error) {
 	pixels := make([][]byte, 0, len(lines))
 
