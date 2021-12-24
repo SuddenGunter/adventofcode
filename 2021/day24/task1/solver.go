4884b11270bd943c0bc6024ec4ce5062705b3927
SuddenGunter <kolomytsev1996@gmail.com>
2021-12-24 15:43:43 +0200

    day24: alu sim

--- 2021/day24/task1/solver.go
+++ 2021/day24/task1/solver.go
@@ -22,30 +22,31 @@ type ALU struct {
 func (a *ALU) runMonadChecker(num int64) int64 {
 	nums := split(num)
 	a.func1(nums.pop(), 1, 11, 1)
-	a.func1(nums.pop(), 11, 11, 1)
+	a.func1(nums.pop(), 1, 11, 11)
 	a.func1(nums.pop(), 1, 14, 1)
-	a.func1(nums.pop(), 11, 11, 1)
-	a.func1(nums.pop(), 2, -8, 26)
-	a.func1(nums.pop(), 9, -5, 26)
-	a.func1(nums.pop(), 7, 11, 1)
-	a.func1(nums.pop(), 11, -13, 26)
-	a.func1(nums.pop(), 6, 12, 1)
-	a.func1(nums.pop(), 15, -1, 26)
-	a.func1(nums.pop(), 7, 14, 1)
-	a.func1(nums.pop(), 1, -5, 26)
-	a.func1(nums.pop(), 8, -4, 26)
-	a.func1(nums.pop(), 6, -8, 26)
+	a.func1(nums.pop(), 1, 11, 11)
+	a.func1(nums.pop(), 26, -8, 2)
+	a.func1(nums.pop(), 26, -5, 9)
+	a.func1(nums.pop(), 1, 11, 7)
+
+	a.func1(nums.pop(), 26, -13, 11)
+	a.func1(nums.pop(), 1, 12, 6)
+	a.func1(nums.pop(), 26, -1, 15)
+	a.func1(nums.pop(), 1, 14, 7)
+	a.func1(nums.pop(), 26, -5, 1)
+	a.func1(nums.pop(), 26, -4, 8)
+	a.func1(nums.pop(), 26, -8, 6)
 
 	return *a.z
 }
 
-func (a *ALU) func1(num int64, v1, v2, v3 int64) {
+func (a *ALU) func1(num, op1, op2, op3 int64) {
 	a.inp(a.w, num)
 	a.mull(a.x, 0)
 	a.add(a.x, a.z)
 	a.modl(a.x, 26)
-	a.divl(a.z, v3)
-	a.addl(a.x, v2)
+	a.divl(a.z, op1)
+	a.addl(a.x, op2)
 	a.eql(a.x, a.w)
 	a.eqll(a.x, 0)
 	a.mull(a.y, 0)
@@ -55,7 +56,7 @@ func (a *ALU) func1(num int64, v1, v2, v3 int64) {
 	a.mul(a.z, a.y)
 	a.mull(a.y, 0)
 	a.add(a.y, a.w)
-	a.addl(a.y, v1)
+	a.addl(a.y, op3)
 	a.mul(a.y, a.x)
 	a.add(a.z, a.y)
 }
