db92e7dc103e5c6ef587fad950b6482ec4bd0735
SuddenGunter <kolomytsev1996@gmail.com>
2022-04-10 13:32:57 +0300

    2015: day1: task2

--- 2015/day1/lib/main.ex
+++ 2015/day1/lib/main.ex
@@ -7,6 +7,10 @@ defmodule Day1.CLI do
   end
 
   defp solve(contents) do
+    IO.write("task #1 solution: ")
     Task1.solution(contents) |> IO.puts()
+
+    IO.write("task #2 solution: ")
+    Task2.solution(contents) |> IO.puts()
   end
 end
