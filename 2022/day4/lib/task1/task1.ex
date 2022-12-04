e9190c0626cff1cd45992689be1d6ea8c3b9740d
SuddenGunter <kolomytsev1996@gmail.com>
2022-12-04 12:45:30 +0200

    2022: day4: task1

--- 2022/day4/lib/task1/task1.ex
+++ 2022/day4/lib/task1/task1.ex
@@ -7,15 +7,15 @@ defmodule Task1 do
     |> Enum.sum()
   end
 
-  defp sortIntervals({l, r}) do
-    l_start = elem(l, 0)
-    r_start = elem(r, 0)
-    if l_start <= r_start, do: {l, r}, else: {r, l}
+  defp sortIntervals({l,r}) do
+    l_len = elem(l, 1) - elem(l, 0)
+    r_len = elem(r, 1) - elem(r, 0)
+    if l_len >= r_len, do: {l,r}, else: {r,l}
   end
 
   defp is_included({l, r}) do
-    l_end = elem(l, 1)
-    r_end = elem(r, 1)
-    if l_end >= r_end, do: 1, else: 0
+    {l_start, l_end} = {elem(l,0), elem(l, 1)}
+    {r_start, r_end} = {elem(r,0), elem(r, 1)}
+    if  l_start <= r_start and  l_end >= r_end, do: 1, else: 0
   end
 end
