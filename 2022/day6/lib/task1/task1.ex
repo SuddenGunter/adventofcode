e60212fcdfbae9dea61afff929ce6657f512e469
SuddenGunter <kolomytsev1996@gmail.com>
2022-12-06 13:29:00 +0200

    2022: day6

--- 2022/day6/lib/task1/task1.ex
+++ 2022/day6/lib/task1/task1.ex
@@ -1,6 +1,16 @@
 defmodule Task1 do
   @spec solution(String.t()) :: integer()
   def solution(contents) do
-    0
+    contents |> String.graphemes() |> firstUniquePos(4)
+  end
+
+  defp firstUniquePos(letters, pos) do
+    firstFour = Enum.take(letters, 4) |> Enum.uniq()
+
+    if length(firstFour) == 4 do
+      pos
+    else
+      firstUniquePos(Enum.drop(letters, 1), pos + 1)
+    end
   end
 end
--- 2022/day6/lib/task2/task2.ex
+++ 2022/day6/lib/task2/task2.ex
@@ -1,6 +1,16 @@
 defmodule Task2 do
   @spec solution(String.t()) :: integer()
   def solution(contents) do
-    0
+    contents |> String.graphemes() |> firstUniquePos(14)
+  end
+
+  defp firstUniquePos(letters, pos) do
+    firstFour = Enum.take(letters, 14) |> Enum.uniq()
+
+    if length(firstFour) == 14 do
+      pos
+    else
+      firstUniquePos(Enum.drop(letters, 1), pos + 1)
+    end
   end
 end
