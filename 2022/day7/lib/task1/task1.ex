e11a9d7a8775172e0ebb24c4b4b19c501d3d82a9
SuddenGunter <kolomytsev1996@gmail.com>
2022-12-08 00:22:40 +0200

    2022: day7: implement task1 for test exmaple

--- 2022/day7/lib/task1/task1.ex
+++ 2022/day7/lib/task1/task1.ex
@@ -2,11 +2,10 @@ defmodule Task1 do
   @spec solution([String.t()]) :: integer()
   def solution(contents) do
     contents
-    # skip cd /
     |> Enum.drop(1)
     |> build_tree()
     |> count_dirs_size()
-    |> dirs_below(100_000)
+    |> dirs_below()
     |> sum_size()
   end
 
@@ -26,44 +25,61 @@ defmodule Task1 do
       else
         case String.split(x) do
           ["dir", dir_name] ->
-            [
-              build_tree(
-                Enum.drop_while(lines, fn l -> l != "$ cd #{dir_name}" end) |> Enum.drop(1),
-                dir_name
-              )
-              | acc
-            ]
+            {:cont,
+             [
+               build_tree(
+                 Enum.drop_while(lines, fn l -> l != "$ cd #{dir_name}" end) |> Enum.drop(1),
+                 dir_name
+               )
+               | acc
+             ]}
 
           ["$", _] ->
-            acc
+            {:halt, acc}
 
           [f_size, f_name] ->
             {fsize_parsed, _} = Integer.parse(f_size)
-            [%Fs.File{name: f_name, size: fsize_parsed} | acc]
+            {:cont, [%Fs.File{name: f_name, size: fsize_parsed} | acc]}
         end
       end
     end)
   end
 
-  @spec solution([Fs.Dir | Fs.File]) :: [Fs.Dir]
-  defp count_dirs_size(x) do
-    x |> Enum.filter(&only_dirs/1) |> Enum.map(fn x -> %{x | size: 10} end)
+  @spec count_dirs_size(%Fs.Dir{}) :: %Fs.Dir{}
+  defp count_dirs_size(root) do
+    sized_children =
+      Enum.map(root.children, fn x ->
+        case x do
+          dir when is_struct(x, Fs.Dir) -> count_dirs_size(dir)
+          file when is_struct(x, Fs.File) -> file
+        end
+      end)
+
+    size = sized_children |> Enum.map(fn x -> x.size end) |> Enum.sum()
+
+    Map.merge(root, %Fs.Dir{name: root.name, size: size, children: sized_children})
   end
 
-  defp only_dirs(f) when is_struct(f, Fs.Dir) do
-    true
+  @spec dirs_below(%Fs.Dir{}, Integer.t()) :: [Fs.Dir]
+  defp dirs_below(root, limit \\ 100_000) do
+    Enum.reduce(root.children, [], fn x, acc ->
+      node =
+        case x do
+          dir when is_struct(x, Fs.Dir) ->
+            if dir.size <= 100_000,
+              do: [dir | dirs_below(dir, limit)],
+              else: dirs_below(dir, limit)
+
+          _ when is_struct(x, Fs.File) ->
+            []
+        end
+
+      [node | acc]
+    end)
+    |> List.flatten()
   end
 
-  defp only_dirs(_) do
-    false
-  end
-
-  @spec solution([Fs.Dir]) :: [Fs.Dir]
-  defp dirs_below(dirs, 100_000) do
-    dirs
-  end
-
-  @spec solution([Fs.Dir]) :: integer()
+  @spec sum_size([Fs.Dir]) :: integer()
   defp sum_size(dirs) do
     dirs
     |> Enum.map(fn x -> x.size end)
