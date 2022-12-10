b3851ba6f2d97f9c639f9225f7447b2f3ab074ae
SuddenGunter <kolomytsev1996@gmail.com>
2022-12-10 20:36:20 +0200

    2022: day10: stream parser

---
+++ 2022/day10/lib/parser.ex
@@ -0,0 +1,24 @@
+defmodule Parser do
+  @spec lines(File.Stream) :: Stream.t()
+  def lines(fstream) do
+    fstream
+    |> Stream.map(&String.trim/1)
+  end
+
+  @spec lines(Stream.t()) :: Stream.t()
+  def data(lines) do
+    lines |> Stream.map(&read_command/1)
+  end
+
+  defp read_command(l) do
+    case String.split(l, " ") do
+      [_ | [arg]] -> {:addx, as_int(arg)}
+      [_] -> {:noop}
+    end
+  end
+
+  defp as_int(str) do
+    {n, _} = Integer.parse(str)
+    n
+  end
+end
---
+++ 2022/day10/lib/task1/task1.ex
@@ -0,0 +1,6 @@
+defmodule Task1 do
+  @spec solution(Stream.t()) :: integer()
+  def solution(_) do
+    0
+  end
+end
