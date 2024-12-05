ed54750b84bdbb34e8f01f4e4ae76b339cab9de8
SuddenGunter <kolomytsev1996@gmail.com>
2024-12-05 02:18:42 +0000

    2024: day 4: p2

--- 2024/day4/lib/task2/task2.ex
+++ 2024/day4/lib/task2/task2.ex
@@ -1,23 +1,74 @@
 defmodule Task2 do
   @spec solution(String.t()) :: integer()
-  def solution(memory) do
-    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)/, memory)
+  def solution(input) do
+    state = parse_input(input)
+
+    Map.get(state, "A")
+    |> Enum.flat_map(fn {row, col} ->
+      [
+        # m _ m
+        # _ a _
+        # s _ s
+        %{
+          "M" => [{row - 1, col - 1}, {row - 1, col + 1}],
+          "S" => [{row + 1, col - 1}, {row + 1, col + 1}]
+        },
+        # s _ m
+        # _ a _
+        # s _ m
+        %{
+          "M" => [{row + 1, col + 1}, {row - 1, col + 1}],
+          "S" => [{row + 1, col - 1}, {row - 1, col - 1}]
+        },
+        # s _ s
+        # _ a _
+        # m _ m
+        %{
+          "M" => [{row + 1, col - 1}, {row + 1, col + 1}],
+          "S" => [{row - 1, col - 1}, {row - 1, col + 1}]
+        },
+        # m _ s
+        # _ a _
+        # m _ s
+        %{
+          "M" => [{row + 1, col - 1}, {row - 1, col - 1}],
+          "S" => [{row + 1, col + 1}, {row - 1, col + 1}]
+        }
+      ]
+    end)
+    |> Enum.filter(fn coords ->
+      Map.keys(coords)
+      |> Enum.all?(fn letter ->
+        all_members(state, coords, letter)
+      end)
+    end)
+    |> Enum.count()
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("", trim: true)
     |> Enum.reduce(
-      {:enabled, 0},
+      {0, 0,
+       %{
+         "X" => MapSet.new(),
+         "M" => MapSet.new(),
+         "A" => MapSet.new(),
+         "S" => MapSet.new()
+       }},
       fn
-        ["do()"], {_, sum} ->
-          {:enabled, sum}
+        "\n", {x, _, state} ->
+          {x + 1, 0, state}
 
-        ["don't()"], {_, sum} ->
-          {:disabled, sum}
-
-        [_, _, _], {:disabled, sum} ->
-          {:disabled, sum}
-
-        [_, a, b], {:enabled, sum} ->
-          {:enabled, sum + String.to_integer(a) * String.to_integer(b)}
+        letter, {x, y, state} ->
+          {x, y + 1, %{state | letter => MapSet.put(Map.get(state, letter), {x, y})}}
       end
     )
-    |> elem(1)
+    |> elem(2)
+  end
+
+  defp all_members(state, coords, letter) do
+    Map.get(coords, letter)
+    |> Enum.all?(fn x -> MapSet.member?(Map.get(state, letter), x) end)
   end
 end
