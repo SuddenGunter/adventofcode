25af527d50fd331856bcfd138b826f0ea3ca2ee6
SuddenGunter <kolomytsev1996@gmail.com>
2024-12-10 00:25:05 +0000

    2024: day9: part2

--- 2024/day9/lib/task2/task2.ex
+++ 2024/day9/lib/task2/task2.ex
@@ -7,7 +7,7 @@ defmodule Task2 do
       |> String.to_charlist()
       |> Enum.map(&(&1 - ?0))
       |> Enum.with_index()
-      |> Enum.reduce({0, 0, [], []}, fn {x, i}, {total_len, file_id, spaces, files} = acc ->
+      |> Enum.reduce({0, 0, [], []}, fn {x, i}, {pos, file_id, spaces, files} = acc ->
         type =
           case rem(i, 2) do
             0 -> :file
@@ -17,36 +17,37 @@ defmodule Task2 do
         if x == 0 do
           acc
         else
+
           case type do
             :file ->
-              {total_len + x, file_id + 1, spaces,
-               [{total_len, total_len + x - 1, file_id}, files]}
+              {pos + x, file_id + 1, spaces,
+               [{pos, x, file_id}, files]}
 
             :space ->
-              {total_len + x, file_id, [{total_len, total_len + x - 1}, spaces], files}
+              {pos + x, file_id, [{pos, x}, spaces], files}
           end
         end
       end)
 
-    move(spaces |> Enum.reverse(), files)
+    # move(spaces |> Enum.reverse(), files)
   end
 
-  defp move(_, []) do
-    moved_files
-  end
+  # defp move(_, []) do
+  #   moved_files
+  # end
 
-  defp move(spaces, files) do
-    {from, to, fid} = Enum.at(0)
-    new_files = Enum.drop(files, 1)
+  # defp move(spaces, [{from,to,fid}]) do
+  #   {from, to, fid} = Enum.at(0)
+  #   new_files = Enum.drop(files, 1)
 
-    # store spaces in min heap by pos
-    # iterate over spaces to find first where val > needed space
-    # remove from min_heap
-    # put file into moved_files
-    # if any space left over put back into min_heap
-    # if file not moved - copy original file into moved_files
+  #   # store spaces in min heap by pos
+  #   # iterate over spaces to find first where val > needed space
+  #   # remove from min_heap
+  #   # put file into moved_files
+  #   # if any space left over put back into min_heap
+  #   # if file not moved - copy original file into moved_files
 
 
-    move(new_spaces, new_files)
-  end
+  #   move(new_spaces, new_files)
+  # end
 end
