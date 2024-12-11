a6f5c2520b9ed990722380569147a64079c83a87
SuddenGunter <kolomytsev1996@gmail.com>
2024-12-11 14:15:59 +0000

    2024: day11: p1

--- 2024/day11/lib/task2/task2.ex
+++ 2024/day11/lib/task2/task2.ex
@@ -1,6 +1,43 @@
 defmodule Task2 do
   @spec solution(String.t()) :: integer()
-  def solution(_input) do
-   0
+  def solution(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.at(0)
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.flat_map(fn x -> blink(x, 75) end)
+    |> Enum.count()
+  end
+
+  def blink(x, 0) do
+    x
+  end
+
+  def blink([], _) do
+    []
+  end
+
+  def blink([x | tail], num) do
+    blink(x, num) ++ blink(tail, num)
+  end
+
+  def blink(x, num) when is_integer(x) do
+    case x do
+      0 ->
+        blink([1], num - 1)
+
+      x ->
+        log10 = :math.log10(x) |> trunc()
+
+        case log10 |> rem(2) do
+          0 ->
+            blink([x * 2024], num - 1)
+
+          _ ->
+            pow10 = :math.pow(10, (log10 / 2) |> round()) |> trunc()
+            blink([div(x, pow10), rem(x, pow10)], num - 1)
+        end
+    end
   end
 end
