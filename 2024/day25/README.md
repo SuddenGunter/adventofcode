36d05181a226e1aaf1258ec3fb0f790a988ac013
SuddenGunter <kolomytsev1996@gmail.com>
2024-12-29 18:19:42 +0000

    2024: day25: init

---
+++ 2024/day25/README.md
@@ -0,0 +1,11 @@
+# Day 25
+
+[Challenge link](https://adventofcode.com/2024/day/25).
+
+How to run:
+
+Put your data into 'data.txt' (in the same directory as this readme file).
+
+```sh
+mix run
+```
---
+++ 2024/day25/lib/main.ex
@@ -0,0 +1,20 @@
+defmodule Day25.CLI do
+  use Application
+
+  def start(_type, _args) do
+    case Input.read("demo.data.txt") do
+      {:ok, contents} -> solve(contents)
+      {:error, reason} -> IO.puts(reason)
+    end
+  end
+
+  defp solve(contents) do
+    IO.write("task #1 solution: ")
+    Task1.solution(contents) |> IO.puts()
+
+    IO.write("task #2 solution: ")
+    Task2.solution(contents) |> IO.puts()
+
+    {:ok, self()}
+  end
+end
