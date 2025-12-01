f3017fc03e28cb61fd0b48ea050027a400f652f0
SuddenGunter <kolomytsev1996@gmail.com>
2025-12-01 01:28:17 +0000

    2025: day1 prep

---
+++ 2025/day1/lib/task1/task1.ex
@@ -0,0 +1,6 @@
+defmodule Task1 do
+  @spec solution(String.t()) :: integer()
+  def solution(input) do
+    input
+  end
+end
---
+++ 2025/day1/lib/task2/task2.ex
@@ -0,0 +1,6 @@
+defmodule Task2 do
+  @spec solution(String.t()) :: integer()
+  def solution(input) do
+    input
+  end
+end
