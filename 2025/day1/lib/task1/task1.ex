634d5753e170da3bdb0593806ba6a35b875efcd5
SuddenGunter <kolomytsev1996@gmail.com>
2025-12-01 11:50:11 +0000

    2025: day1

--- 2025/day1/lib/task1/task1.ex
+++ 2025/day1/lib/task1/task1.ex
@@ -1,6 +1,44 @@
 defmodule Task1 do
   @spec solution(String.t()) :: integer()
   def solution(input) do
-    input
+    start = 50
+    rotate(input |> String.trim() |> String.split("\n"), start, 0)
+  end
+
+  defp rotate([], _current_pos, total_zeroes) do
+    total_zeroes
+  end
+
+  defp rotate([h | tail], current_pos, total_zeroes) do
+    new_pos = add(h, current_pos)
+
+    {pos, zeroes} =
+      cond do
+        new_pos == 100 || new_pos == 0 ->
+          {0, total_zeroes + 1}
+
+        new_pos > 99 ->
+          {new_pos - 100, total_zeroes}
+
+        new_pos < 1 ->
+          {100 + new_pos, total_zeroes}
+
+        true ->
+          {new_pos, total_zeroes}
+      end
+
+    rotate(tail, pos, zeroes)
+  end
+
+  defp add("L" <> moves, current_pos) do
+    {num, _} = Integer.parse(moves)
+    real_moves = rem(num, 100)
+    current_pos - real_moves
+  end
+
+  defp add("R" <> moves, current_pos) do
+    {num, _} = Integer.parse(moves)
+    real_moves = rem(num, 100)
+    current_pos + real_moves
   end
 end
--- 2025/day1/lib/task2/task2.ex
+++ 2025/day1/lib/task2/task2.ex
@@ -1,6 +1,46 @@
 defmodule Task2 do
   @spec solution(String.t()) :: integer()
   def solution(input) do
-    input
+    start = 50
+    rotate(input |> String.trim() |> String.split("\n"), start, 0)
+  end
+
+  defp rotate([], _current_pos, total_zeroes) do
+    total_zeroes
+  end
+
+  defp rotate([h | tail], current_pos, total_zeroes) do
+    {new_pos, clicks_while_rotating} = add(h, current_pos)
+
+    {pos, zeroes} =
+      cond do
+        new_pos == 100 || new_pos == 0 ->
+          {0, total_zeroes + 1}
+
+        new_pos > 99 ->
+          total = if current_pos != 0, do: total_zeroes + 1, else: total_zeroes
+          {new_pos - 100, total}
+
+        new_pos < 1 ->
+          total = if current_pos != 0, do: total_zeroes + 1, else: total_zeroes
+          {100 + new_pos, total}
+
+        true ->
+          {new_pos, total_zeroes}
+      end
+
+    rotate(tail, pos, zeroes + clicks_while_rotating)
+  end
+
+  defp add("L" <> moves, current_pos) do
+    {num, _} = Integer.parse(moves)
+    real_moves = rem(num, 100)
+    {current_pos - real_moves, div(num, 100)}
+  end
+
+  defp add("R" <> moves, current_pos) do
+    {num, _} = Integer.parse(moves)
+    real_moves = rem(num, 100)
+    {current_pos + real_moves, div(num, 100)}
   end
 end
