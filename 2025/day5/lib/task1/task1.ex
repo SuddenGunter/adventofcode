72a782ad0b6d837557bd694155232a679ff87c89
SuddenGunter <kolomytsev1996@gmail.com>
2025-12-05 12:47:01 +0000

    2025: day5 parse

--- 2025/day5/lib/task1/task1.ex
+++ 2025/day5/lib/task1/task1.ex
@@ -1,6 +1,39 @@
 defmodule Task1 do
   @spec solution(String.t()) :: integer()
-  def solution(_input) do
+  def solution(input) do
+    {intervals, ingridients} =
+      parse(input)
+      |> IO.inspect()
+
     0
+  end
+
+  defp parse(input) do
+    String.trim(input)
+    |> String.split("\n")
+    |> Enum.reduce({[], []}, fn x, {intervals, ingridients} ->
+      cond do
+        String.contains?(x, "-") ->
+          {l, r} =
+            String.split(x, "-")
+            |> Enum.map(fn n ->
+              {num, _} = Integer.parse(n)
+              num
+            end)
+            |> List.to_tuple()
+
+          {[{l, r} | intervals], ingridients}
+
+        x == "" ->
+          {intervals, ingridients}
+
+        true ->
+          {num, _} = Integer.parse(x)
+          {intervals, [num | ingridients]}
+      end
+    end)
+    |> Tuple.to_list()
+    |> Enum.map(fn x -> Enum.reverse(x) end)
+    |> List.to_tuple()
   end
 end
